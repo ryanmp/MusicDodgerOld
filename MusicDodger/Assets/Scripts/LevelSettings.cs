0475c52e44e83a2bd69815a3dae720a1a1409e23
Ryan Phillips <wordsandcolor@gmail.com>
2015-08-17 21:56:37 -0700

    Done for today
    this version is turning out to be much cleaner than the last

--- MusicDodger/Assets/Scripts/LevelSettings.cs
+++ MusicDodger/Assets/Scripts/LevelSettings.cs
@@ -1,19 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 using SimpleJSON;
 
 public class LevelSettings : MonoBehaviour {
 
-	public GameObject enemyA;
-	public GameObject enemyB;
-	public GameObject enemyC;
+	public GameObject[] enemy_prefabs;
 
 	public static JSONNode testData; 
 	public static TextAsset filejson;
 	public static string fileNamejson = "Music/Levels/";
 	public string songName = "demo1";
 
-	private static int track_idx = 0;
+	private static List<int> track_idxs = new List<int>();
+
+	public Camera main_camera;
+
+	private static Vector2 bounds;
+
 
 	// Use this for initialization
 	void Start () {
@@ -21,39 +25,61 @@ public class LevelSettings : MonoBehaviour {
 		filejson = (TextAsset) Resources.Load(fileNamejson + songName);
 		testData = JSON.Parse(filejson.text); 
 
-		/*
-		Debug.Log (
-			testData[0][0]["actual_time"].AsFloat
-			);
-		*/
+		// make an index for each track (for incrementing through the notes while playing the song)
+		for (int i = 0; i < testData.Count; i++){
+			track_idxs.Add(0);
+		}
+
+		// let's figure out the max and min world coords currently visible with our screen dimensions
+		//Debug.Log (Screen.height + " " + Screen.width);
+		float camera_distance = main_camera.transform.position.z;
+		Vector3 upper_right = main_camera.ScreenToWorldPoint(
+			new Vector3(0f,0f,camera_distance));
+		Vector3 bottom_left = main_camera.ScreenToWorldPoint(
+			new Vector3(Screen.width,Screen.height,camera_distance));
+
+		bounds = new Vector2(upper_right.x,upper_right.y);
 
 	}
 
-	void GenEnemy(){
-		float x_pos = Random.Range(-5.0f,5.0f);
-		float y_pos = Random.Range(-5.0f,5.0f);
-		
+	void GenEnemy(int i){
+
+		// set initial position
+		float x_pos = Random.Range(-bounds.x,bounds.x);
+		float y_pos = bounds.y;
+
+		// pick prefab
+		int prefab_idx = i%enemy_prefabs.Length; // in case we have more tracks than enemy types
+		GameObject clone = Instantiate(enemy_prefabs[prefab_idx], new Vector3(x_pos, y_pos, 0), Quaternion.identity) as GameObject;
+
+		// set velocity
 		float x_vel = Random.Range(-5.0f,5.0f); 
-		float y_vel = Random.Range(-5.0f,5.0f); 
-		
-		GameObject clone = Instantiate(enemyA, new Vector3(x_pos, y_pos, 0), Quaternion.identity) as GameObject;
+		float y_vel = Random.Range(-5.0f,-10.0f); 
 		Rigidbody2D rb = clone.GetComponent ("Rigidbody2D") as Rigidbody2D;
 		rb.velocity = new Vector2(x_vel,y_vel);
-	}
 
+		// set color
+		SpriteRenderer sr = clone.GetComponent ("SpriteRenderer") as SpriteRenderer;
+
+		float red = Random.Range(0.0f,1.0f);
+		float blue = Random.Range(0.0f,1.0f);
+		sr.color = new Color(red,1.0f,blue);
+
+
+	}
 	
 	// Update is called once per frame
 	void Update () {
-
-		float current_trigger = testData[4][track_idx]["actual_time"].AsFloat;
-
-		if (Time.timeSinceLevelLoad >= current_trigger){
-
-			Debug.Log (current_trigger);
-			GenEnemy();
-			track_idx++;
-
+		for (int i = 0; i < testData.Count; i++){
+			float current_trigger = testData[i][track_idxs[i]]["actual_time"].AsFloat;
+			if (Time.timeSinceLevelLoad >= current_trigger){
+				Debug.Log (current_trigger);
+				GenEnemy(i);
+				track_idxs[i]++;
+			}
 		}
+
+
 
 	}
 }
